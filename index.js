34fd01730a8893e30833465b7cbe54eda89f5fef
vicente candela perez <vicentecandelaperez@gmail.com>
2022-07-20 13:11:11 +0200

    segunda fase del webserver

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express()
+
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials', function(err){})
+
+
 
 // Servir contenido estatico
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+
+    res.render('home',{
+        nombre: 'Vicente Candela', 
+        titulo: 'Aprendiendo node.js'
+    });
+
+});
+
 app.get('/generic', (req, res) => {
 
-    res.sendFile(__dirname+"/public/generic.html");
+    res.sendFile(__dirname+"/views/generic.hbs");
 
 });
 
